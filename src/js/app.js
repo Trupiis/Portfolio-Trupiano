a7406905f7652cd1ac9a2dc04b01dc0884d97e2b
Elian Trupiano <eliantrupianogg@gmail.com>
2025-08-02 19:50:08 -0300

    TECNOLOGIAS SLIDER ADD

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+let logos= document.getElementById("logos-slide").cloneNode(true)
+let logos2= document.getElementById("logos-slide-2").cloneNode(true)
+document.getElementById("logos").appendChild(logos)
+
+console.log("cargando")
